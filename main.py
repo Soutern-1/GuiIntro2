a717daee87e5229fbe83fe6406ccb84d861d0324
Soutern-1 <81514298+Soutern-1@users.noreply.github.com>
2024-10-19 11:01:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 from tkinter import *
 
 
+def openWindow():
+
+    calendar = Tk()
+
+    calendar.geometry("1000x1000")
+
+    calendar.config(background="white")
+
+    calendar.title("Year")
+
+    # calendar.calendar(enterededyear)
+
+
 if __name__ == "__main__":
 
     gui = Tk()
 
-    gui.geometry("200x200")
+    gui.geometry("500x500")
 
     gui.config(background="black")
 
-    gui.title("Calender")
+    gui.title("Calendar")
 
-    cal = Label(gui, text = "Calendar", bg= "dark gray", font=("times new roman",28,"bold"))
+    cal = Label(gui, text = "Calendar", bg= "dark gray", font=("times new roman",22,"bold"))
 
-    cal.grid(row = 3, column=3)
+    cal.grid(row = 1, column=1, padx=30,pady=5)
 
+    info = Label(gui, text = "Enter a year below to view the calendar", bg= "dark gray", font=("times new roman",14,"bold"))
+    info.grid(row = 2, column=1, pady=5)
+
+
+    enteryear = Entry(gui, background="white" ) 
+    enteryear.grid(row = 3, column=1, padx=30,pady=10)
+
+    confirm = Button(gui, text = "Confirm Year", bg= "dark gray", font=("times new roman",14,"bold" ), command = openWindow)
+    confirm.grid(row=4,column=1,pady=10, )
+
+    exit = Button(gui, text = "Exit", bg= "dark gray", font=("times new roman",14,"bold"), command=exit)
+    exit.grid(row=5, column=1, pady=10)
     gui.mainloop()
+    enterededyear = int(enteryear.get())
+
+
+    # year = int(enteryear.get)
+
+
+
+
